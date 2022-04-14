db308064630104815d4e2444dd9e5cff2895d711
Vladislav Nikolaev <vladisnotlove@gmail.com>
2022-04-14 16:32:22 +0300

    Make basic landing for desktop

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,174 @@
+const colorPrime = '#CEE44A';
+const arrowBorderRadius = 40; //px
+
+
+// UTILS
+
+function throttle(func, wait, options) {
+	var context, args, result;
+	var timeout = null;
+	var previous = 0;
+	if (!options) options = {};
+	var later = function() {
+		previous = options.leading === false ? 0 : Date.now();
+		timeout = null;
+		result = func.apply(context, args);
+		if (!timeout) context = args = null;
+	};
+	return function() {
+		var now = Date.now();
+		if (!previous && options.leading === false) previous = now;
+		var remaining = wait - (now - previous);
+		context = this;
+		args = arguments;
+		if (remaining <= 0 || remaining > wait) {
+			if (timeout) {
+				clearTimeout(timeout);
+				timeout = null;
+			}
+			previous = now;
+			result = func.apply(context, args);
+			if (!timeout) context = args = null;
+		} else if (!timeout && options.trailing !== false) {
+			timeout = setTimeout(later, remaining);
+		}
+		return result;
+	};
+};
+
+function addArc(pathData, radius) {
+	var reL = /^L ?([\d.\-+]+) ([\d.\-+]+) ?/,
+		newPathData, curXY, curDir, newXY, newDir,
+		sweepFlag, arcXY, arcStartXY;
+
+	function getDir(xy1, xy2) {
+		if (xy1.x === xy2.x) {
+			return xy1.y < xy2.y ? 'd' : 'u';
+		} else if (xy1.y === xy2.y) {
+			return xy1.x < xy2.x ? 'r' : 'l';
+		}
+		throw new Error('Invalid data');
+	}
+
+	function captureXY(s, x, y) {
+		newXY = {x: +x, y: +y};
+		return '';
+	}
+
+	function offsetXY(xy, dir, offsetLen, toBack) {
+		return {
+			x: xy.x + (dir === 'l' ? -offsetLen : dir === 'r' ? offsetLen : 0) * (toBack ? -1 : 1),
+			y: xy.y + (dir === 'u' ? -offsetLen : dir === 'd' ? offsetLen : 0) * (toBack ? -1 : 1)
+		};
+	}
+
+	pathData = pathData.trim().replace(/,/g, ' ').replace(/\s+/g, ' ')
+	.replace(/^M ?([\d.\-+]+) ([\d.\-+]+) ?/, function(s, x, y) {
+		curXY = {x: +x, y: +y};
+		return '';
+	});
+	if (!curXY) { throw new Error('Invalid data'); }
+	newPathData = 'M' + curXY.x + ' ' + curXY.y;
+
+	while (pathData) {
+		newXY = null;
+		pathData = pathData.replace(reL, captureXY);
+		if (!newXY) { throw new Error('Invalid data'); }
+
+		newDir = getDir(curXY, newXY);
+		if (curDir) {
+			arcStartXY = offsetXY(curXY, curDir, radius, true);
+			arcXY = offsetXY(curXY, newDir, radius);
+			sweepFlag =
+				curDir === 'l' && newDir === 'u' ? '1' :
+					curDir === 'l' && newDir === 'd' ? '0' :
+						curDir === 'r' && newDir === 'u' ? '0' :
+							curDir === 'r' && newDir === 'd' ? '1' :
+								curDir === 'u' && newDir === 'l' ? '0' :
+									curDir === 'u' && newDir === 'r' ? '1' :
+										curDir === 'd' && newDir === 'l' ? '1' :
+											curDir === 'd' && newDir === 'r' ? '0' :
+												null;
+			if (!sweepFlag) { throw new Error('Invalid data'); }
+			newPathData += 'L' + arcStartXY.x + ' ' + arcStartXY.y +
+				'A ' + radius + ' ' + radius + ' 0 0 ' + sweepFlag + ' ' + arcXY.x + ' ' + arcXY.y;
+		}
+
+		curXY = newXY;
+		curDir = newDir;
+	}
+	newPathData += 'L' + curXY.x + ' ' + curXY.y;
+	return newPathData;
+}
+
+
+// MAIN
+
+window.addEventListener('load', () => {
+
+	// add arrows
+
+	LeaderLine.positionByWindowResize = false;
+
+	const getLineOptions = (start, end) => {
+		const startY = start.offsetTop;
+		const endY = end.offsetTop;
+
+		const isDifferentRows = Math.abs(startY - endY) > 2;
+
+		return {
+			color: colorPrime,
+			size: 2,
+			endPlug: "arrow2",
+			dash: {len: 9, gap: 6},
+			path: "grid",
+			...(isDifferentRows ? {
+				startSocket: endY > startY ? "bottom" : "top",
+				endSocket: startY < endY ? "top" : "bottom",
+			} : {
+				startSocket: "auto",
+				endSocket: "auto",
+			}),
+		}
+	}
+
+	const fixLineStyles = () => {
+		document.querySelectorAll('.leader-line .leader-line-line-path:first-child').forEach(line => {
+			line.setAttribute('d', addArc(line.getAttribute('d'), arrowBorderRadius));
+		});
+	}
+
+	const howSteps = document.querySelectorAll('.how__step');
+	const lines = [];
+
+	for (let i = 1; i < howSteps.length; i++) {
+		const step = howSteps[i];
+		const prevStep = howSteps[i - 1];
+
+		lines.push(new LeaderLine(
+			prevStep,
+			step,
+			getLineOptions(prevStep, step)
+		));
+	}
+	fixLineStyles();
+
+	window.addEventListener("resize", throttle(
+		() => {
+		lines.forEach((line, index) => {
+			const options = getLineOptions(line.start, line.end);
+			line.setOptions(options);
+			line.position();
+		})
+		fixLineStyles();
+	},
+		300
+	))
+
+
+
+});
+
 // $(document).ready(function(){
 //
 //   // автовысота для overlay
