08eafdd7703695c966855e102e360db8f934c764
Vladislav Nikolaev <vladisnotlove@gmail.com>
2022-04-22 21:36:06 +0300

    Remove errors + fix safari not scroll bug

--- src/js/components/youtube-embedded.js
+++ src/js/components/youtube-embedded.js
@@ -1,3 +1,4 @@
+import cssVars from '../styles/cssVars';
 
 window.addEventListener('load', () => {
 
@@ -5,16 +6,38 @@ window.addEventListener('load', () => {
 		iframe.src = iframe.src;
 	}
 
+	document.querySelectorAll("[data-youtube-embedded]").forEach(trigger => {
+		const target = document.querySelector(trigger.dataset.target);
+
+		trigger.addEventListener("click", () => {
+			if (target) {
+				document.querySelectorAll(".youtube-embedded").forEach(other => {
+					other.classList.remove("open");
+					other.hidden = true;
+				});
+				target.classList.add("open");
+				target.hidden = false;
+			}
+		})
+	})
+
 	document.querySelectorAll(".youtube-embedded").forEach(container => {
 		const overlay = container.querySelector(".youtube-embedded__overlay");
 		const iframe = container.querySelector("iframe");
 
 		iframe.src = iframe.dataset.src;
+		container.hidden = true;
 
 		if (overlay) {
+			let hiddenTimeout = -1;
 			overlay.addEventListener("click", () => {
+				clearTimeout(hiddenTimeout);
 				container.classList.remove("open");
 				stopVideo(iframe);
+
+				hiddenTimeout = setTimeout(() => {
+					container.hidden = true;
+				}, cssVars.animationDisappear * 1000);
 			})
 		}
 	})
