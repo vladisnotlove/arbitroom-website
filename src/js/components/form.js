732e3896323cf77554c6fb128cd3b2d9a094cc8e
Vladislav Nikolaev <vladisnotlove@gmail.com>
2022-04-26 22:19:34 +0300

    Add form validation for registration + add validation to all pages

--- src/js/components/form.js
+++ src/js/components/form.js
@@ -1,6 +1,14 @@
 import isEmail from 'validator/es/lib/isEmail';
 
 
+/**
+ *
+ * @param {string} str
+ */
+const trim = (str) => {
+	return str.replace(/ +/g, " ").trim();
+}
+
 // UTILS
 
 /**
@@ -21,6 +29,13 @@ const validateField = (field, input) => {
 			}
 		}
 	}
+	else {
+		if (isCheckbox && !value || !isCheckbox && !value.trim()) {
+			return {
+				isValid: true,
+			}
+		}
+	}
 
 	if (field.dataset.email !== undefined) {
 		const message = field.dataset.emailMessage;
@@ -35,7 +50,7 @@ const validateField = (field, input) => {
 	if (field.dataset.minMaxLength !== undefined) {
 		const message = field.dataset.minMaxLengthMessage;
 		const [min,max] = field.dataset.minMaxLength.split(":").map(str => parseFloat(str));
-		const length = value.trim().length;
+		const length = trim(value).length;
 
 		if (!isCheckbox && (length > max || length < min)) {
 			return {
@@ -45,18 +60,84 @@ const validateField = (field, input) => {
 		}
 	}
 
+	if (field.dataset.notRegex !== undefined) {
+		const message = field.dataset.notRegexMessage;
+		const regex = new RegExp(field.dataset.notRegex);
 
-	if (field.dataset.regex !== undefined) {
-		const message = field.dataset.regexMessage;
-		const regex = new RegExp(field.dataset.regex);
-
-		if (!isCheckbox && !value.match(regex)) {
+		if (!isCheckbox && value.match(regex)) {
 			return {
 				isValid: false,
 				message: message,
 			}
 		}
 	}
+
+
+	for (let i = -1; i < 3; i++) {
+		let suffix = "-" + i;
+		if (i === -1) {
+			suffix = ""
+		}
+
+		const regex = field.dataset["regex" + suffix];
+
+		if (regex !== undefined) {
+			const message = field.dataset["regexMessage" + suffix];
+
+			if (!isCheckbox && !value.match(regex)) {
+				return {
+					isValid: false,
+					message: message,
+				}
+			}
+		}
+
+	}
+
+	if (field.dataset.confirmPassword !== undefined) {
+		const message = field.dataset.confirmPasswordMessage;
+		const passwordField = document.querySelector(field.dataset.confirmPassword);
+		const passwordInput = passwordField.querySelector("input");
+
+		if (!isCheckbox && input.value !== passwordInput.value) {
+			return {
+				isValid: false,
+				message: message,
+			}
+		}
+	}
+
+	if (field.dataset.maxWordCount !== undefined) {
+		const message = field.dataset.maxWordCountMessage;
+		const maxCount = parseFloat(field.dataset.maxWordCount);
+		const count = trim(value).split(" ").length;
+
+		if (!isCheckbox && count > maxCount) {
+			return {
+				isValid: false,
+				message: message,
+			}
+		}
+	}
+
+	if (field.dataset.wordMinMaxLength !== undefined) {
+		const message = field.dataset.wordMinMaxLengthMessage;
+		const [min,max] = field.dataset.wordMinMaxLength.split(":").map(str => parseFloat(str));
+		const words = trim(value).split(" ");
+
+		for (let i = 0; i < words.length; i++) {
+			const word = words[i];
+			const length = word.length;
+
+			if (!isCheckbox && (length > max || length < min)) {
+				return {
+					isValid: false,
+					message: message,
+				}
+			}
+		}
+	}
+
 
 	return {
 		isValid: true,
@@ -78,6 +159,11 @@ window.addEventListener('load', () => {
 			fieldUpdaters.forEach(updateField => updateField());
 		}
 
+		let fieldTrimmers = [];
+		const trimAllFields = () => {
+			fieldTrimmers.forEach(trimField => trimField());
+		}
+
 		form.querySelectorAll("[data-form-field]").forEach(field => {
 			const input = field.querySelector("input");
 			const messageContainer = field.querySelector("[data-form-field-message]");
@@ -88,14 +174,19 @@ window.addEventListener('load', () => {
 
 				if (!isFieldValid) {
 					field.classList.add("error");
-					messageContainer.textContent = message;
+					if (messageContainer) messageContainer.textContent = message;
 				}
 				else {
 					field.classList.remove("error");
-					messageContainer.textContent = "";
+					if (messageContainer) messageContainer.textContent = "";
 				}
 			}
 			fieldUpdaters.push(updateField);
+
+			const trimField = () => {
+				input.value = trim(input.value);
+			}
+			fieldTrimmers.push(trimField);
 
 			input.addEventListener("input", () => {
 				if (state.isSubmitted) updateField();
@@ -106,6 +197,7 @@ window.addEventListener('load', () => {
 			state.isSubmitted = true;
 			state.isValid = true;
 			updateAllFields();
+			if (state.isValid) trimAllFields();
 			if (!state.isValid) e.preventDefault();
 		});
 
