315dad5901f04e9a647feb5f3de3fcd23f82c9f4
Vladislav Nikolaev <vladisnotlove@gmail.com>
2022-08-26 04:35:16 +0300

    Redesign footer + partly redesign menu

--- src/js/components/goals.js
+++ src/js/components/goals.js
@@ -1,8 +1,7 @@
 import  'sticksy';
 
 
-const focusTopPercents = 0.25;
-const focusBottomPercents = 0.45;
+const focusCenterPercents = 0.3;
 const minOpacity = 0.2;
 const maxOpacity = 1;
 
@@ -20,40 +19,58 @@ const updateOpacity = (elem) => {
 	const isInView = center > 0 && center < window.innerHeight;
 	if (!isInView) {
 		elem.style.opacity = minOpacity;
-		elem.classList.remove("highlight");
 		return;
 	}
 
-	const focusTop = focusTopPercents * window.innerHeight;
-	const focusBottom = focusBottomPercents * window.innerHeight;
-	const focusHeight = focusBottom - focusTop
-	const focusCenter = focusTop + focusHeight * 0.5;
-
-	const isInFocus = center > focusTop && center < focusBottom;
+	const focusCenter = focusCenterPercents * window.innerHeight;
+	const isInFocus = focusCenter > rect.top && focusCenter < rect.bottom;
 
 	if (isInFocus) {
 		elem.style.opacity = maxOpacity;
-		elem.classList.add("highlight");
 		return;
 	}
 
 	const diff = Math.abs(center - focusCenter);
-	const coeff = 1 - diff / cards.clientHeight;
-
-
-	console.log(diff);
-	console.log(cards.clientHeight);
+	const coeff = (1 - diff / (window.innerHeight - focusCenter)) * 0.85;
 
 	elem.style.opacity = coeff > 1 ?
 		maxOpacity :
 		Math.max(coeff, minOpacity);
-
-	elem.classList.remove("highlight");
 }
+
+/**
+ *
+ * @param {NodeListOf<Element>} elems
+ */
+const updateHighlight = (elems) => {
+	let minDiff;
+	let closestElem;
+
+	elems.forEach(elem => {
+		const rect = elem.getBoundingClientRect();
+		const center = rect.top + rect.height * 0.5;
+		const focusCenter = focusCenterPercents * window.innerHeight;
+		const diff = Math.abs(center - focusCenter);
+
+		if (!minDiff || diff < minDiff) {
+			closestElem = elem;
+			minDiff = diff;
+		}
+
+		elem.classList.remove("highlight");
+	});
+
+	if (closestElem && closestElem.style.opacity > 0.65) {
+		closestElem.classList.add("highlight");
+	}
+}
+
 
 window.addEventListener("load", () => {
 	const updateAll = () => {
-		document.querySelectorAll(".goals__card").forEach(updateOpacity);
+		const cards = document.querySelectorAll(".goals__card");
+		cards.forEach(updateOpacity);
+		updateHighlight(cards);
 	}
 
 	updateAll();
