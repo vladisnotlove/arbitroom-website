5e76088ed4383d391e2d3689bc5c28362e7906be
Vladislav Nikolaev <vladisnotlove@gmail.com>
2022-11-26 22:42:52 +0300

    Complete steps

--- src/js/components/stepper.js
+++ src/js/components/stepper.js
@@ -5,6 +5,7 @@ window.addEventListener("load", () => {
 	document.querySelectorAll("[data-stepper]").forEach(stepper => {
 		const steps = stepper.querySelectorAll("[data-stepper-item]");
 		const nextBtns = stepper.querySelectorAll("[data-stepper-next]");
+		const stepBtns = stepper.querySelectorAll("[data-stepper-to]");
 
 		if (!steps) {
 			log.elementNotFound("[data-stepper-item]");
@@ -15,6 +16,18 @@ window.addEventListener("load", () => {
 			nextBtn.addEventListener("click", () => {
 				const activeStepIndex = [...steps].findIndex(step => step.classList.contains("active"));
 				const nextActiveStepIndex = activeStepIndex + 1;
+				const nextActiveStep = steps[nextActiveStepIndex];
+
+				if (nextActiveStep) {
+					steps.forEach(step => step.classList.remove("active"));
+					nextActiveStep.classList.add("active");
+				}
+			})
+		});
+
+		stepBtns.forEach(stepBtn => {
+			stepBtn.addEventListener("click", () => {
+				const nextActiveStepIndex = parseInt(stepBtn.dataset.stepperTo);
 				const nextActiveStep = steps[nextActiveStepIndex];
 
 				if (nextActiveStep) {
